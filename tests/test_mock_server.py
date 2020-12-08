c06de247d32ca0ad83046f8c499f8ba63baa9afe
M. Mert Yildiran <mehmet@up9.com>
2020-12-08 19:29:24 +0300

    Remove the Docker related code from feature tests

--- tests/test_mock_server.py
+++ tests/test_mock_server.py
@@ -10,7 +10,6 @@
 
 import sys
 import signal
-import subprocess
 import pytest
 from os import path
 from unittest.mock import patch
@@ -24,42 +23,21 @@ PROGRAM = 'chupeta'
 templates = [
     'templates/template.json.j2'
 ]
-test_parameters = []
-for template in templates:
-    test_parameters.append(('default', template))
-    test_parameters.append(('docker', template))
 
 
-@pytest.mark.parametrize(('mode', 'template'), test_parameters)
+@pytest.mark.parametrize(('template'), templates)
 class TestFeatures:
     def setup_method(self):
-        mode = self._item.callspec.getparam('mode')
         template = self._item.callspec.getparam('template')
 
         self.mock_server_process = None
         __location__ = path.abspath(path.dirname(__file__))
         template_path = path.join(__location__, template)
 
-        mode = 'default'  # TODO: Remove this line
-        if mode == 'default':
-            testargs = [PROGRAM, template_path]
-            with patch.object(sys, 'argv', testargs):
-                self.mock_server_process = Process(target=initiate, args=())
-                self.mock_server_process.start()
-        elif mode == 'docker':
-            template_filename = template_path.split('/')[-1]
-            cmd = [
-                'docker',
-                'run',
-                '--init',
-                '--network',
-                'host',
-                '-v',
-                '%s:/%s' % (template_path, template_filename),
-                PROGRAM,
-                '/%s' % template_filename
-            ]
-            self.mock_server_process = subprocess.run(cmd)
+        testargs = [PROGRAM, template_path]
+        with patch.object(sys, 'argv', testargs):
+            self.mock_server_process = Process(target=initiate, args=())
+            self.mock_server_process.start()
 
         signal.signal(signal.SIGUSR1, signal_handler)
         signal.pause()
@@ -67,7 +45,7 @@ class TestFeatures:
     def teardown_method(self):
         self.mock_server_process.terminate()
 
-    def test_ping_ports(self, mode, template):
+    def test_ping_ports(self, template):
         ports = (8001, 8002)
         for port in ports:
             result, _ = tcping('localhost', port)
