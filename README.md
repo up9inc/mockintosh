63dcb4e63dc590daa24651346644e7291e7ddc7d
M. Mert Yildiran <mehmet@up9.com>
2020-12-12 18:20:46 +0300

    Add test_users test case to TestCommon class and fix some issues in the configs

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ you should be seeing `{"hello": "world"}` response.
 Running directly:
 
 ```bash
-chupeta tests/configs/hbs/config.json
+chupeta tests/configs/json/hbs/common/config.json
 ```
 
 or as a Docker container:
--- tests/test_configs.py
+++ tests/test_configs.py
@@ -6,9 +6,12 @@
     :synopsis: End-to-end tests related to mock server's itself.
 """
 
-import pytest
+import os
 
-from utilities import signal_handler, tcping, run_mock_server
+import pytest
+import requests
+
+from utilities import tcping, run_mock_server
 
 configs = [
     'configs/json/hbs/common/config.json',
@@ -17,9 +20,12 @@ configs = [
     'configs/yaml/j2/common/config.yaml'
 ]
 
+SRV1 = os.environ.get('SRV1', 'http://localhost:8001')
+SRV2 = os.environ.get('SRV2', 'http://localhost:8002')
+
 
 @pytest.mark.parametrize(('config'), configs)
-class TestCommon:
+class TestCommon():
     def setup_method(self):
         config = self._item.callspec.getparam('config')
         self.mock_server_process = run_mock_server(config)
@@ -33,3 +39,7 @@ class TestCommon:
             result, _ = tcping('localhost', port)
             if not result:
                 raise AssertionError("Port %d is closed!" % port)
+
+    def test_users(self, config):
+        resp = requests.get(SRV1 + '/users', headers={'Host': 'service1.example.com'})
+        assert 200 == resp.status_code
