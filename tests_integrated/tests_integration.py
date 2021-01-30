ded1d00ca102171b0ead403e12bbcc076117c952
Andrey Pokhilko <apc4@ya.ru>
2021-01-30 21:06:32 +0300

    Add header count check

--- tests_integrated/tests_integration.py
+++ tests_integrated/tests_integration.py
@@ -567,6 +567,7 @@ class IntegrationTests(unittest.TestCase):
         oas = resp.json()
         self.assertEqual(29, len(oas['paths']))
         self.assertEqual(3, len(oas['paths']['/qstr-matching1']['get']['parameters']))
+        self.assertEqual(3, len(oas['paths']['/header-matching1']['get']['parameters']))
 
         resp = requests.get(SRV6 + '/__admin/oas')
         resp.raise_for_status()
