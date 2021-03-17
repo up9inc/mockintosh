4a01c1abfc775338d051c788dbf5bc69f352010d
M. Mert Yildiran <mehmet@up9.com>
2021-03-17 23:46:21 +0300

    Add a test script to test the helpers

---
+++ tests/test_helpers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: __init__
+    :synopsis: Contains classes that tests the helpers.
+"""
+
+import sys
+import pytest
+
+from mockintosh.methods import _urlsplit
+
+
+class TestHelpers():
+
+    def test_urlsplit(self):
+        scheme, netloc, path, query, fragment = _urlsplit('https://example.com/path/resource.txt?a=b&c=d#fragment')
+        assert scheme == 'https'
+        assert netloc == 'example.com'
+        assert path == '/path/resource.txt'
+        assert query == 'a=b&c=d'
+        assert fragment == 'fragment'
