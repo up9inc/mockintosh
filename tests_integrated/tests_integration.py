fae60044305c9bcdb39fac065aa31a71ea82d258
Andrey Pokhilko <apc4@ya.ru>
2021-04-21 10:05:06 +0300

    Alfter test a bit

--- tests_integrated/tests_integration.py
+++ tests_integrated/tests_integration.py
@@ -956,6 +956,26 @@ class IntegrationTests(unittest.TestCase):
         headers = dict(msgs[0].headers() if msgs[0].headers() else [])
         self.assertEqual("justvalue", headers['name'].decode())
 
+    @pytest.mark.kafka
+    def test_kafka_producer_chained(self):
+        # clean the log
+        resp = httpx.delete(MGMT + '/async/consumers/chain1-validating', verify=False)
+        resp.raise_for_status()
+
+        resp = httpx.post(MGMT + '/async/producers/chain1-on-demand', verify=False)
+        resp.raise_for_status()
+
+        for _ in range(5):
+            resp = httpx.get(MGMT + '/async/consumers/chain1-validating', verify=False)
+            resp.raise_for_status()
+            msgs = resp.json()['log']['entries']
+            if not msgs:
+                time.sleep(1)
+                continue
+            self.assertEqual(1, len(msgs))
+        else:
+            self.fail("Did not capture the message")
+
 
 def kafka_consume_expected(topic, group='0', timeout=1.0, mfilter=lambda x: True, validator=lambda x: None,
                            after_subscribe=lambda: None):
