2ae02e47667f90db0259cb57cb198a1add6f3a74
M. Mert Yildiran <mehmet@up9.com>
2021-06-01 09:47:21 +0300

    Remove random sampling of OpenAPI directory from the test script

--- tests/test-openapi-transpiler.sh
+++ tests/test-openapi-transpiler.sh
@@ -6,22 +6,3 @@ coverage run --parallel -m mockintosh $petstore -c dev.json json && \
 coverage run --parallel -m mockintosh $petstore -c dev.yaml yaml && \
 coverage run --parallel -m mockintosh $petstore -c dev.yaml && \
 jsonschema -i dev.json mockintosh/schema.json || exit 1
-
-total=20
-i=0
-
-find ./tests/openapi-directory/APIs -type f | shuf -n $total | while read -d $'\n' file
-do
-    ((i++))
-    echo -e "\n${i}/${total}"
-    if prance validate $file ; then
-        echo -e "Testing $file"
-        coverage run --parallel -m mockintosh $file -c dev.json json || exit 1
-
-        # TODO: Throws avalidation error in case of `exclusiveMaximum` is bool.
-        # TODO: Setting `--validator Draft4Validator` causes more issues
-        jsonschema -i dev.json mockintosh/schema.json # || exit 1
-    else
-        echo -e "Passing $file"
-    fi
-done
