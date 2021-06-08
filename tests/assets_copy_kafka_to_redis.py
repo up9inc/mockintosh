e37e75192e6a9b30afa94db4eded4fc495dd20fe
M. Mert Yildiran <mehmet@up9.com>
2021-06-08 13:19:45 +0300

    Revert "Remove `key` and `headers` fields while generating the Redis config"
    This reverts commit a9583a6c7657134c4589cbe010cb4ec74c73aea5.

--- tests/assets_copy_kafka_to_redis.py
+++ tests/assets_copy_kafka_to_redis.py
@@ -3,34 +3,18 @@
 import yaml
 
 
-def remove_a_key(d, remove_key) -> dict:
-    if isinstance(d, dict):
-        for key in list(d.keys()):
-            if key == remove_key:
-                del d[key]
-            else:
-                remove_a_key(d[key], remove_key)
-    elif isinstance(d, list):
-        for x in d:
-            remove_a_key(x, remove_key)
-
-
 # Edit tests/configs/yaml/hbs/redis/config.yaml
 data = yaml.safe_load(open('tests/configs/yaml/hbs/redis/config.yaml', 'r'))
 
 data['services'][0]['type'] = 'redis'
 data['services'][0]['address'] = "{{env 'REDIS_ADDR' 'localhost:6379'}}"
 data['services'][0]['actors'][9]['produce']['headers']['counter'] = "{{counter 'redisCounter'}}"
-data['services'][0]['actors'][4]['produce']['value'] = "value5 and {{consumed.value}}"
 
 data['services'][2]['type'] = 'redis'
 data['services'][2]['address'] = 'localhost:6380'
 
 data['services'][3]['type'] = 'redis'
 data['services'][3]['address'] = 'localhost:6381'
-
-remove_a_key(data, 'key')
-remove_a_key(data, 'headers')
 
 yaml.dump(data, open('tests/configs/yaml/hbs/redis/config.yaml', 'w'), sort_keys=False)
 
@@ -40,8 +24,5 @@ data = yaml.safe_load(open('tests/configs/yaml/hbs/redis/config_error.yaml', 'r'
 
 data['services'][0]['type'] = 'redis'
 data['services'][0]['address'] = "{{env 'AMQP_ADDR' 'localhost:6379'}}"
-
-remove_a_key(data, 'key')
-remove_a_key(data, 'headers')
 
 yaml.dump(data, open('tests/configs/yaml/hbs/redis/config_error.yaml', 'w'), sort_keys=False)
