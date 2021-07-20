6ff24e54bc30caff38df5fbc8c4e966d08d2fb1a
Andrey Pokhilko <apc4@ya.ru>
2021-07-20 21:26:20 +0300

    Demonstrate approach

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,16 +5,23 @@
 .. module:: __init__
     :synopsis: Contains classes that tests the helpers.
 """
+import logging
+import re
+import unittest
+import uuid
 
+import graphql
 import pytest
 
 from mockintosh import start_render_queue
-from mockintosh.helpers import _urlsplit
-from mockintosh.j2.methods import env
-from mockintosh.constants import JINJA
 from mockintosh.config import (
     ConfigAsyncService
 )
+from mockintosh.constants import JINJA, PYBARS
+from mockintosh.hbs.methods import reg_ex
+from mockintosh.helpers import _urlsplit
+from mockintosh.j2.methods import env
+from mockintosh.templating import RenderingTask
 
 
 class TestHelpers:
@@ -58,3 +65,93 @@ class TestHelpers:
         assert config_async_service.address == 'someothervalue'
 
         job.kill()
+
+
+class TemplateMapper(object):
+    matches = {}
+
+    def __call__(self, match):
+        new_id = '_' + str(uuid.uuid4()).replace('-', '')
+        self.matches[new_id] = match.group(0)
+        return new_id
+
+
+def graphql_match(query, template):
+    query = graphql.parse(query)
+    query = graphql.print_ast(query).strip()
+    logging.info("Normalized query:\n%s", query)
+
+    mapper = TemplateMapper()
+    template = re.sub(r'({{[^{}]*}})', mapper, template)
+    logging.info("Template after replace:\n%s", template)
+
+    template = graphql.parse(template)
+    template = graphql.print_ast(template).strip()
+    logging.info("Template after normalized:\n%s", template)
+
+    template = '^' + re.escape(template) + '$'
+
+    for k, v in mapper.matches.items():
+        template = template.replace(k, v)
+
+    task = RenderingTask(PYBARS, template, inject_methods=[reg_ex], inject_objects={"scope": None, 'key': None})
+    template, context = task.render_handlebars()
+
+    logging.info("Template after render:\n%s", template)
+
+    return re.match(template, query)
+
+
+class TestGraphQL(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        logging.basicConfig(level=logging.DEBUG, format='[%(relativeCreated)d %(name)s %(levelname)s] %(message)s')
+
+    def test_1(self):
+        query = """
+        query HeroNameAndFriends {
+            hero(
+                    where: {name: {_eq: "hello"}, _and: {age: {_gt: 3}}}
+                ) {
+                    name
+                    age    
+                    friends {      name    }
+            }
+        }
+        """
+
+        template = r"""
+        query HeroNameAndFriends {
+            hero(
+                    where: {name: {_eq: "{{regEx '.+'}}"}, _and: {age: {_gt: {{regEx '.+'}}}}}
+                ) {
+                    name
+                    age    
+                    friends { name }
+            }
+        }
+        """
+
+        self.assertTrue(graphql_match(query, query))
+
+        self.assertTrue(graphql_match(query, template))
+
+    def test_2(self):
+        query = """
+        {
+            hero(
+                    where: {name: {_eq: "hello"}}
+                ) 
+        }
+        """
+        template = """
+        {
+            hero(
+                    where: {
+                        name: {_eq: "{{regEx '.+'}}"}
+                    }
+                ) 
+        }
+        """
+        self.assertTrue(graphql_match(query, template))
