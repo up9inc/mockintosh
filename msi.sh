4c6beee6007ac51c3c7306811fc294284cc9a618
Andrey Pokhilko <apc4@ya.ru>
2021-10-02 16:51:14 +0300

    Build it

--- msi.sh
+++ msi.sh
@@ -1,13 +1,18 @@
 #! /bin/bash -xe
 
 python3 setup.py bdist_wheel
+
+# build wheels for projects that don't do it
+pip wheel -w dist paho-mqtt pybars4 PyMeta3
+
+# download appropriate native-including wheel
 pip download --no-deps --platform win_amd64 --dest dist ruamel.yaml.clib
 
 python3 -m venv --clear build/venv
 source build/venv/bin/activate
 
 pip install homebrew-pypi-poet wheel pynsist
-pip install ruamel.yaml.clib six~=1.15
+pip install ruamel.yaml.clib
 pip install -e .[cloud]
 python3 msi.py
 
